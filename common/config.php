cb6e54b317bb50f91f8d389bf6631459b0c98474
Pare06 <jacopoparenti06@gmail.com>
2025-02-19 21:41:16 +0100

    file random

---
+++ common/config.php
@@ -0,0 +1,14 @@
+<?php
+    $conn_config = [
+        "host" => "localhost",
+        "user" => "root",
+        "pass" => null,
+        "db" => "progetto_matematica"
+    ];
+
+    mysqli_report(MYSQLI_REPORT_OFF);
+
+    $conn = new mysqli($conn_config["host"], $conn_config["user"], $conn_config["pass"], $conn_config["db"]);
+    if ($conn->connect_errno) {
+        $conn = null;
+    }
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    require_once "imports.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>boh</title>
+</head>
+<body>
+    
+</body>
+</html>
